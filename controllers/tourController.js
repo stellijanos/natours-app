6481896952a7d1c229a4c598d0b18c787ca3f2d5
Janos Stelli <stellijanos23@gmail.com>
2024-07-24 14:51:02 +0300

    updating documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -5,12 +5,12 @@ exports.createTour = async (req, res) => {
         // const newTour = new Tour({});
         // newTour.save();
 
-        const newTour = await Tour.create(req.body);
+        const tour = await Tour.create(req.body);
 
         res.status(201).json({
             status: 'success',
             data: {
-                tour: newTour,
+                tour,
             },
         });
     } catch (err) {
@@ -61,13 +61,25 @@ exports.getTour = async (req, res) => {
     }
 };
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>',
-        },
-    });
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
 
 exports.deleteTour = (req, res) => {
