5f4b304274cabf21adc517726edd0f41648eb9ee
Janos Stelli <stellijanos23@gmail.com>
2024-07-24 14:18:53 +0300

    another way of creating documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,22 +1,26 @@
 const Tour = require('../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-    if (!req.body.name || !req.body.price) {
-        return res.status(400).json({
+exports.createTour = async (req, res) => {
+    try {
+        // const newTour = new Tour({});
+        // newTour.save();
+
+        const newTour = await Tour.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    } catch (err) {
+        res.status(400).json({
             status: 'fail',
-            message: 'Missing name or price',
+            data: {
+                message: 'Invalid data sent!',
+            },
         });
     }
-    next();
-};
-
-exports.createTour = (req, res) => {
-    res.status(201).json({
-        status: 'success',
-        // data: {
-        //     tour: newTour,
-        // },
-    });
 };
 
 exports.getAllTours = (req, res) => {
