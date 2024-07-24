212eadeeea7e067df4a43a084166b80f8362b024
Janos Stelli <stellijanos23@gmail.com>
2024-07-24 11:34:18 +0300

    creating a simple tour model

--- server.js
+++ server.js
@@ -1,7 +1,39 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({path : './config.env'});
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('DB connection successful'));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'], // validator
+        unique: true 
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price'],
+    },
+});
+
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+
 
 
 const port = process.env.PORT || 3000;
