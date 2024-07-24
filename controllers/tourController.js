5b3e2a132102ec52f2c3c024d371b28d2fa50c87
Janos Stelli <stellijanos23@gmail.com>
2024-07-24 15:35:21 +0300

    deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -65,7 +65,7 @@ exports.updateTour = async (req, res) => {
     try {
         const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
             new: true,
-            runValidators: true
+            runValidators: true,
         });
 
         res.status(200).json({
@@ -82,9 +82,18 @@ exports.updateTour = async (req, res) => {
     }
 };
 
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'success',
-        data: null,
-    });
+exports.deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'success',
+            data: null,
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
 };
