45627c964b02e4cd18cbc85316bf320b5b3cf192
Janos Stelli <stellijanos23@gmail.com>
2024-07-24 13:46:12 +0300

    creating documents and testing the model

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ mongoose
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
+        useUnifiedTopology: true,
     })
     .then(() => console.log('DB connection successful'));
 
@@ -18,11 +19,11 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'], // validator
-        unique: true 
+        unique: true,
     },
     rating: {
         type: Number,
-        default: 4.5
+        default: 4.5,
     },
     price: {
         type: Number,
@@ -30,11 +31,21 @@ const tourSchema = new mongoose.Schema({
     },
 });
 
-
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+    name: 'The Park Camper',
+    price: 997
+});
 
-
+testTour
+    .save()
+    .then((doc) => {
+        console.log(doc);
+    })
+    .catch((err) => {
+        console.log(`ERROR: ${err}`);
+    });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
