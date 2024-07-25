a55b8d7916cc444c08f7cbd03ace90d2a2bf7a67
Janos Stelli <stellijanos23@gmail.com>
2024-07-25 11:11:01 +0300

    making the api better: sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -28,20 +28,30 @@ exports.getAllTours = async (req, res) => {
         console.log(req.query);
 
         // BUILD QUERY
-        // 1) Filtering
+        // 1A) Filtering
 
         const queryObj = { ...req.query };
         const excludedFields = ['page', 'sort', 'limit', 'fields'];
         excludedFields.forEach((el) => delete queryObj[el]);
 
-        // 2) Advanced filtering
+        // 1B) Advanced filtering
         let queryStr = JSON.stringify(queryObj);
         queryStr = queryStr.replace(
             /\b(gte|gt|lte|lt)\b/g,
             (match) => `$${match}`,
         );
 
-        const query = Tour.find(JSON.parse(queryStr));
+        let query = Tour.find(JSON.parse(queryStr));
+
+        // 2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            console.log(sortBy);
+            query = query.sort(sortBy);
+            // sort('price ratingsAverage')
+        } else {
+            query.sort('-createdAt');
+        }
 
         // EXECUTE QUERY
         const tours = await query;
