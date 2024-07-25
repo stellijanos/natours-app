d6ffdb9e0aa1b40629499ec785ee6bbf60bd1d26
Janos Stelli <stellijanos23@gmail.com>
2024-07-25 10:56:19 +0300

    making the api better: advanced filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,7 +25,32 @@ exports.createTour = async (req, res) => {
 
 exports.getAllTours = async (req, res) => {
     try {
-        const tours = await Tour.find();
+        console.log(req.query);
+
+        // BUILD QUERY
+        // 1) Filtering
+
+        const queryObj = { ...req.query };
+        const excludedFields = ['page', 'sort', 'limit', 'fields'];
+        excludedFields.forEach((el) => delete queryObj[el]);
+
+        // 2) Advanced filtering
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(
+            /\b(gte|gt|lte|lt)\b/g,
+            (match) => `$${match}`,
+        );
+
+        const query = Tour.find(JSON.parse(queryStr));
+
+        // EXECUTE QUERY
+        const tours = await query;
+
+        // const tours = await Tour.find()
+        //     .where('duration')
+        //     .equals(5)
+        //     .where('difficulty')
+        //     .equals('easy');
 
         res.status(200).json({
             status: 'success',
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
+
 mongoose
     .connect(DB, {
         useNewUrlParser: true,
