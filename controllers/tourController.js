d471b4b2d20896063e1fcf4260770415a02db644
Janos Stelli <stellijanos23@gmail.com>
2024-07-26 11:34:47 +0300

    Aggregation Pipeline: matching and Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -111,3 +111,47 @@ exports.deleteTour = async (req, res) => {
         });
     }
 };
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([
+            {
+                $match: { rating: { $gte: 4.5 } },
+            },
+            {
+                $group: {
+                    _id: { $toUpper: '$difficulty' },
+                    numTours: { $sum: 1 },
+                    numRatings: { $sum: '$ratingsQuantity' },
+                    avgRating: { $avg: '$rating' },
+                    avgPrice: { $avg: '$price' },
+                    minPrice: { $min: '$price' },
+                    maxPrice: { $max: '$price' },
+                },
+            },
+            {
+                $sort: {
+                    avgPrice: 1,
+                },
+            },
+            // {
+            //     $match: {
+            //         _id: {
+            //             $ne: 'EASY',
+            //         },
+            //     },
+            // },
+        ]);
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats,
+            },
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err,
+        });
+    }
+};
