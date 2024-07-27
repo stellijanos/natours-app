497c1d9e2b5fe502f645ba69227188c083f558e0
Janos Stelli <stellijanos23@gmail.com>
2024-07-27 07:00:44 +0300

    Query Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -55,6 +55,10 @@ const tourSchema = new mongoose.Schema(
             select: false,
         },
         startDates: [Date],
+        secretTour: {
+            type: Boolean,
+            default: false,
+        },
     },
     {
         toJSON: {
@@ -86,6 +90,21 @@ tourSchema.pre('save', function (next) {
 //     console.log(doc);
 //     next();
 // });
+
+// QUERY MIDDLEWARE
+
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } });
+
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`QUery took: ${Date.now() - this.start} milliseconds!`);
+    console.log(docs);
+    next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
