ec31e08d4e595b5ed8c2a518416b0743b8f2b96b
Janos Stelli <stellijanos23@gmail.com>
2024-07-27 18:05:44 +0300

    errors outside express: unhandled rejections

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
-
 mongoose
     .connect(DB, {
         useNewUrlParser: true,
@@ -17,6 +16,14 @@ mongoose
     .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`App running on port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log(err.name, '> ', err.message);
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    server.close(() => {
+        process.exit(1);
+    });
 });
