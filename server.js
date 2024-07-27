1c054fd184ab5d046504155a4e919bf581598ad0
Janos Stelli <stellijanos23@gmail.com>
2024-07-27 18:18:10 +0300

    catching uncaught exceptions

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! Shutting down...');
+    console.log(err.name, '. msg: ', err.message);
+    process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
