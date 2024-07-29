5d86bdbf7b9b18f20b9efe07229a1a3fe9a35dbb
Janos Stelli <stellijanos23@gmail.com>
2024-07-29 14:45:57 +0300

    preventing parameter pollution

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // limit requests from the same IP
 const limiter = rateLimit({
-    max: 3,
+    max: 100,
     windowMs: 60 * 60 * 1000,
     message: {
         status: 'fail',
@@ -46,6 +47,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS
 app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price',
+        ],
+    }),
+);
 
 // Servinng static files
 app.use(express.static(`${__dirname}/public`));
