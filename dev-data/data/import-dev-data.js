409c2ff453d85a2eb8a4919372657f3ea5553689
Janos Stelli <stellijanos23@gmail.com>
2024-07-30 11:29:42 +0300

    modelling locations (geospatial data)

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -17,7 +17,7 @@ mongoose
     .then(() => console.log('DB connection successful'));
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+    fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'),
 );
 
 // IMPORT DATA INTO DB
