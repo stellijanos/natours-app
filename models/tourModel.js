3d254147e766a61d54af8ac51118edd22b1efe29
Janos Stelli <stellijanos23@gmail.com>
2024-07-30 18:46:06 +0300

    populating reviews

--- models/tourModel.js
+++ models/tourModel.js
@@ -176,15 +176,15 @@ tourSchema.pre(/^find/, function (next) {
 });
 
 // tourSchema.post(/^find/, function (docs, next) {
-    // console.log(`QUery took: ${Date.now() - this.start} milliseconds!`);
-    // console.log(docs);
-    // next();
+// console.log(`QUery took: ${Date.now() - this.start} milliseconds!`);
+// console.log(docs);
+// next();
 // });
 
 // AGGREGATION MIDDLEWARE
 tourSchema.pre('aggregate', function (next) {
     this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-    console.log(this.pipeline());
+    // console.log(this.pipeline());
     next();
 });
 
