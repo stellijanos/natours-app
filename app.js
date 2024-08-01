a33ff97d816c6d628e03983c87d805dc9d538c87
Janos Stelli <stellijanos23@gmail.com>
2024-08-01 12:45:03 +0300

    Setting up Pug in Express

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 //  1) GLOBAL MIDDLEWARES
+
+// Servinng static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //  set security HTTP Headers
 app.use(helmet());
@@ -63,9 +70,6 @@ app.use(
     }),
 );
 
-// Servinng static files
-app.use(express.static(`${__dirname}/public`));
-
 // Test middleware
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
@@ -74,6 +78,11 @@ app.use((req, res, next) => {
 });
 
 // 2) ROUTES
+
+app.get('/', (req, res) => {
+    res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
