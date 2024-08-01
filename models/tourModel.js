e6034464a46960723a71b38b95e653f95e7a96e7
Janos Stelli <stellijanos23@gmail.com>
2024-08-01 10:53:23 +0300

    Geospatial Aggregation: Calculating Distances

--- models/tourModel.js
+++ models/tourModel.js
@@ -195,11 +195,11 @@ tourSchema.pre(/^find/, function (next) {
 // });
 
 // AGGREGATION MIDDLEWARE
-tourSchema.pre('aggregate', function (next) {
-    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-    // console.log(this.pipeline());
-    next();
-});
+// tourSchema.pre('aggregate', function (next) {
+//     this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+//     console.log(this.pipeline());
+//     next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
