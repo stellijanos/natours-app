5eafa803348f0497b1d2a0d9430724314b104355
Janos Stelli <stellijanos23@gmail.com>
2024-08-02 11:21:45 +0300

    build the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -14,8 +14,19 @@ exports.getOverView = catchAsync(async (req, res, next) => {
     });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+    // 1) get the data, for the requested tour (including reviews and guides)
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        fields: 'review rating user',
+    });
+
+    // 2) buld template
+    console.log()
+
+    // 3) render template using data from 1)
     res.status(200).render('tour', {
         title: 'The Forest Hiker Tour',
+        tour
     });
-};
+});
