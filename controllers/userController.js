ded240e846699319b12955ae4dffc67dbdb32d74
Janos Stelli <stellijanos23@gmail.com>
2024-08-08 17:55:19 +0300

    Resizing Images

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,21 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'public/img/users/');
-    },
-    filename: (req, file, cb) => {
-        const ext = file.mimetype.split('/')[1];
-        cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-    },
-});
+// const multerStorage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'public/img/users/');
+//     },
+//     filename: (req, file, cb) => {
+//         const ext = file.mimetype.split('/')[1];
+//         cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//     },
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
@@ -31,6 +34,20 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+    if (!req.file) return next();
+
+    req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+    sharp(req.file.buffer)
+        .resize(500, 500)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/users/${req.file.filename}`);
+
+    next();
+};
 
 const filterObj = (obj, ...allowedFields) => {
     const newObj = {};
@@ -55,7 +72,6 @@ exports.createUser = (req, res) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-
     // 1) create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
         return next(
@@ -68,7 +84,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
     // filtered out unwanted field names that are not allowed to be updated
     const filteredBody = filterObj(req.body, 'name', 'email');
-    if(req.file) filteredBody.photo = req.file.filename
+    if (req.file) filteredBody.photo = req.file.filename;
 
     // 3) Update user document
     const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
