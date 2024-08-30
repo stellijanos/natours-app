de479166f41106a06f637674f482c3e23ea9a06a
Janos Stelli <stellijanos23@gmail.com>
2024-08-30 18:49:32 +0300

    Testing for Secure HTTPS Connections

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,18 +11,17 @@ const signToken = (id) =>
         expiresIn: process.env.JWT_EXPIRES_IN,
     });
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
     const token = signToken(user._id);
-    const cookieOptions = {
+
+    res.cookie('jwt', token, {
         expires: new Date(
             Date.now() +
                 process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
         ),
         httpOnly: true,
-    };
-    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
-
-    res.cookie('jwt', token, cookieOptions);
+        secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    });
 
     // Remove password from the output
     user.password = undefined;
@@ -49,7 +48,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 
     await new Email(newUser, url).sendWelcome();
 
-    createSendToken(newUser, 201, res);
+    createSendToken(newUser, 201, req, res);
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -68,7 +67,7 @@ exports.login = catchAsync(async (req, res, next) => {
     }
 
     // 3) if everything is ok, send token to client
-    createSendToken(user, 200, res);
+    createSendToken(user, 200, req, res);
 });
 
 exports.logout = (req, res) => {
@@ -205,7 +204,7 @@ exports.resetPassword = catchAsync(async (req, res, next) => {
     // 3) update changedPasswordAt property for the user
 
     // 4) log the user in, send JWT
-    createSendToken(user, 200, res);
+    createSendToken(user, 200, req, res);
 });
 
 exports.updatePassword = catchAsync(async (req, res, next) => {
@@ -227,7 +226,7 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
     // User.findByIdAndUpdate will NOT work as intended!
 
     // log user in, send JWT
-    createSendToken(user, 200, res);
+    createSendToken(user, 200, req, res);
 });
 
 // only for rendered pages, no errors
